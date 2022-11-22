be9ba6fec4c6afcfbeca9817c2982113934b5b55
Filip Mårtensson <fenwikk@protonmail.com>
2022-11-22 13:36:03 +0100

    Schedule

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -14,9 +14,71 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Naviation Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+          colorScheme:
+              const ColorScheme.dark().copyWith(secondary: Colors.green)),
+      home: const Navigation(),
+    );
+  }
+}
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int currentPageIndex = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: DropdownButton(items: const [
+        DropdownMenuItem(
+          child: Text("T1C"),
+        )
+      ], onChanged: (e) => {})),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.commute),
+            label: 'Commute',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.calendar_view_day),
+            icon: Icon(Icons.calendar_view_day_outlined),
+            label: 'Schema',
+          ),
+        ],
       ),
-      home: const LessonsPage(),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        child: <Widget>[
+          Container(
+            color: Colors.red,
+            alignment: Alignment.center,
+            child: const Text('Page 1'),
+          ),
+          Container(
+            color: Colors.green,
+            alignment: Alignment.center,
+            child: const Text('Page 2'),
+          ),
+          const Schedule()
+        ][currentPageIndex],
+      ),
     );
   }
 }
--- app/lib/schedule.dart
+++ app/lib/schedule.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:app/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -23,8 +24,8 @@ class Lesson {
   final String room;
 
   final int dayOfWeek;
-  final String from;
-  final String to;
+  final DateTime from;
+  final DateTime to;
 
   const Lesson(
       {required this.id,
@@ -44,20 +45,20 @@ class Lesson {
       teacher: json["teacher"],
       room: json["room"],
       dayOfWeek: json["dayOfWeek"],
-      from: json["from"],
-      to: json["to"],
+      from: DateTime.parse(json["from"]),
+      to: DateTime.parse(json["to"]),
     );
   }
 }
 
-class LessonsPage extends StatefulWidget {
-  const LessonsPage({super.key});
+class Schedule extends StatefulWidget {
+  const Schedule({super.key});
 
   @override
-  State<LessonsPage> createState() => Lessons();
+  State<Schedule> createState() => Lessons();
 }
 
-class Lessons extends State<LessonsPage> {
+class Lessons extends State<Schedule> {
   late Future<List<Lesson>> futureLessons;
 
   @override
@@ -68,32 +69,139 @@ class Lessons extends State<LessonsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("hi"),
-        ),
-        body: const Text("hi"));
+    return FutureBuilder<List<Lesson>>(
+      future: futureLessons,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return RefreshIndicator(
+            onRefresh: _pullRefresh,
+            child: SingleChildScrollView(
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                child: Wrap(
+                    alignment: WrapAlignment.start,
+                    direction: Axis.horizontal,
+                    children: snapshot.data!.map<Widget>((currentLesson) {
+                      double screenWidth = MediaQuery.of(context).size.width;
+
+                      return SizedBox(
+                        width: screenWidth,
+                        child: Card(
+                          semanticContainer: true,
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 5),
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 10),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                top: BorderSide(
+                                    color: HexColor(
+                                        currentLesson.colors?.background ??
+                                            "fff"),
+                                    width: 3),
+                              ),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.baseline,
+                              textBaseline: TextBaseline.alphabetic,
+                              children: [
+                                Container(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(6, 4, 6, 3),
+                                  decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(2)),
+                                      color: Colors.black26),
+                                  child: Text(
+                                      "${currentLesson.from.hour}:${currentLesson.from.minute}"),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 7, horizontal: 7),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(bottom: 3),
+                                        child: Text(
+                                          currentLesson.name,
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16),
+                                        ),
+                                      ),
+                                      Text(
+                                          "${currentLesson.teacher != "" ? "${currentLesson.teacher}; " : ""}${currentLesson.room}"),
+                                    ],
+                                  ),
+                                ),
+                                Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Container(
+                                      padding:
+                                          const EdgeInsets.fromLTRB(6, 4, 6, 3),
+                                      decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(2)),
+                                          color: Colors.black26),
+                                      child: Text(
+                                          "${currentLesson.to.hour}:${currentLesson.to.minute}"),
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList()),
+              ),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        // By default, show a loading spinner.
+        return const Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+
+  Future<void> _pullRefresh() async {
+    Future<List<Lesson>> freshLessons = fetchLessons();
+    setState(() {
+      futureLessons = Future.value(freshLessons);
+    });
   }
 }
+
+class DateFormat {}
 
 Future<List<Lesson>> fetchLessons() async {
   final response =
       await http.get(Uri.https('gtg.seabird.digital', "/api/schedule/lessons", {
     "selectionGuid": "MTJhNTBiNjktNjhhZS1mMTNhLWEzYjEtNGM2NGZhZmE1ZDhi",
-    "week": "42"
+    "week": "42",
+    "day": "1",
+    "parsed": "true"
   }));
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
+    var data = jsonDecode(response.body)["data"];
+
     List<Lesson> lessons = [];
-    for (var i = 0; i < jsonDecode(response.body).length; i++) {
-      lessons.add(Lesson.fromJson(jsonDecode(response.body)));
+    for (var i = 0; i < data.length; i++) {
+      lessons.add(Lesson.fromJson(data[i]));
     }
 
     return lessons;
   } else {
-    var thing = response.body;
     // If the server did not return a 200 OK response,
     // then throw an exception.
     throw Exception('Failed to load Lesson');
