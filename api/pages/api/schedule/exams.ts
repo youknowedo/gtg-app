d107677e5d2cb3e0fff527dc4d89270deebef99d
Filip Mårtensson <fenwikk@protonmail.com>
2022-11-24 20:07:51 +0100

    Fully working exam schedule

--- api/pages/api/schedule/exams.ts
+++ api/pages/api/schedule/exams.ts
@@ -7,7 +7,20 @@ import Errors from "../../../lib/api/errors";
 const weekExp = /v.([1-5]){1,2}/;
 const dayExp = /(.){3}[ ]/;
 
-const exams = async (req: NextApiRequest, res: ApiResponse<unknown>) => {
+type ExamData = {
+    date: Date;
+    exams: Exam[];
+};
+
+type Exam = {
+    type?: string;
+    name?: string;
+    teacher?: string;
+
+    registered?: Date;
+};
+
+const exams = async (req: NextApiRequest, res: ApiResponse<ExamData[]>) => {
     if (req.method == "GET") {
         const schedule = await fetch(
             "https://www.gtc.com/provschema/kalender.asp?klass=T1c"
@@ -34,7 +47,112 @@ const exams = async (req: NextApiRequest, res: ApiResponse<unknown>) => {
             if (notEmptyTexts.length > 0) texts.push(notEmptyTexts);
         }
 
-        res.status(200).json({ data: texts });
+        const today = new Date();
+        const firstDateText = texts[0][0];
+        const date = new Date(
+            today.getFullYear(),
+            +firstDateText.split("/")[1] - 1,
+            +firstDateText.split("/")[0] - 1
+        );
+        if (date > today) date.setFullYear(date.getFullYear() - 1);
+
+        const examData: ExamData[] = [];
+        for (const d of days) {
+            const ts = d.contents().toArray();
+
+            const content = $(d).text().trim();
+            if (content == "" || weekExp.test(content)) continue;
+
+            date.setDate(date.getDate() + 1);
+
+            let exams: Exam[] = [];
+            for (let i = 1; i < ts.length; i++) {
+                let e: Exam = {};
+
+                if (
+                    i == 1 &&
+                    ($(ts).get(0) as HTMLElement | undefined)?.tagName == "span"
+                )
+                    i += 2;
+
+                while (
+                    ($(ts).get(i) as HTMLElement | undefined)?.tagName == "br"
+                )
+                    i++;
+
+                if (
+                    ($(ts).get(i) as HTMLElement | undefined)?.tagName == "span"
+                ) {
+                    e.type = $($(ts).get(i)).text().trim();
+
+                    do i++;
+                    while (
+                        ($(ts).get(i) as HTMLElement | undefined)?.tagName ==
+                        "br"
+                    );
+                }
+
+                if (
+                    ($(ts).get(i) as HTMLElement | undefined)?.tagName ==
+                    undefined
+                ) {
+                    e.name = $($(ts).get(i)).text().trim();
+
+                    do i++;
+                    while (
+                        ($(ts).get(i) as HTMLElement | undefined)?.tagName ==
+                        "br"
+                    );
+                }
+
+                if (($(ts).get(i) as HTMLElement | undefined)?.tagName == "i") {
+                    e.teacher = $($(ts).get(i)).text().trim();
+
+                    do i++;
+                    while (
+                        ($(ts).get(i) as HTMLElement | undefined)?.tagName ==
+                        "br"
+                    );
+                }
+
+                if (
+                    ($(ts).get(i) as HTMLElement | undefined)?.tagName == "div"
+                ) {
+                    e.teacher = $($(ts).get(i)).text().trim();
+
+                    do i++;
+                    while (
+                        ($(ts).get(i) as HTMLElement | undefined)?.tagName ==
+                        "br"
+                    );
+                }
+
+                i++;
+                if (
+                    ($(ts).get(i) as HTMLElement | undefined)?.tagName == "div"
+                ) {
+                    e.registered = new Date(
+                        $($(ts).get(i)).text().trim().substring(22)
+                    );
+
+                    do i++;
+                    while (
+                        ($(ts).get(i) as HTMLElement | undefined)?.tagName ==
+                        "br"
+                    );
+                }
+
+                if (e.type || e.name || e.teacher) exams.push(e);
+            }
+
+            if (exams.length > 0)
+                examData.push({
+                    date: new Date(date),
+                    exams,
+                });
+        }
+
+        res.status(200).json({ data: examData });
 
         return;
     }
